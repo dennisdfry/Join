79843bc3c9e297ebd90551d823181a71610973dc
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-07-15 18:41:43 +0200

    added css and js

---
+++ src/js/contacts.js
@@ -0,0 +1,10 @@
+let BASE_URL = "https://join-19628-default-rtdb.firebaseio.com";
+
+
+//function init{}
+//funtion addContact{}
+//async function getDataJson{}
+//function editContact
+//function deleteContact
+// function includeHTML
+//function getImages{}
