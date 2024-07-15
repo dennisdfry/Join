86f8f4d34f0f9e5b1259853341ce97ca72499480
M1roel <ppfautsch@yahoo.de>
2024-07-15 17:59:08 +0200

    beta-test for rendering sites with includeHTML script

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      let file = element.getAttribute("w3-include-html");
+      let resp = await fetch(file);
+      if (resp.ok) {
+        element.innerHTML = await resp.text();
+      } else {
+        element.innerHTML = 'Page not found';
+      }
+    }
+  }
 
-let positionOfContent = document.getElementById('content');
+  function changeSite(page) {
+    document.querySelector('.main-content').setAttribute('w3-include-html', page);
+    includeHTML();
+  }
 
-function changeSite(page){
-    positionOfContent.src = page;
-    
-}
+  document.addEventListener('DOMContentLoaded', () => {
+    includeHTML();
+  });
