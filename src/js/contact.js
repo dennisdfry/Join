d511de0e3393fdfcd8e7f80a4f743745f51dd9b1
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-07-16 19:02:45 +0200

    @contact.js contacts addable

--- src/js/contact.js
+++ src/js/contact.js
@@ -10,16 +10,20 @@ let allContacts = {
 async function init() {
   try {
     await getData("/");
+    setupFormListener();
   } catch (error) {
-    console.log("error");
+    console.log("Error:", error);
   }
 }
 
 async function getData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
   let responseToJson = await response.json();
-  let keys = Object.keys(responseToJson);
+  updateContacts(responseToJson);
+}
 
+function updateContacts(responseToJson) {
+  let keys = Object.keys(responseToJson);
   for (let i = 0; i < keys.length; i++) {
     let contact = responseToJson[keys[i]];
     allContacts.names.push(contact.name);
@@ -27,4 +31,48 @@ async function getData(path = "") {
     allContacts.phones.push(contact.phone);
     allContacts.images.push(contact.img);
   }
+  console.log(allContacts);
 }
+
+async function postData(contact) {
+  try {
+    let response = await fetch(BASE_URL + ".json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    });
+
+    let responseToJson = await response.json();
+    console.log("Erfolgreich hochgeladen:", responseToJson);
+
+    allContacts.names.push(contact.name);
+    allContacts.mails.push(contact.mail);
+    allContacts.phones.push(contact.phone);
+    allContacts.images.push(contact.img);
+
+    await getData("/");
+  } catch (error) {
+    console.error("error");
+  }
+}
+function handleFormSubmit(event) {
+    event.preventDefault();  
+  
+
+    const newContact = {
+      name: document.getElementById("name").value,
+      mail: document.getElementById("mail").value,
+      phone: document.getElementById("phone").value,
+      img: document.getElementById("img").value,
+    };
+  
+    postData(newContact);
+    document.getElementById("contactForm").innerHTML ="";
+  }
+  
+  function setupFormListener() {
+    const form = document.getElementById("contactForm");
+    form.addEventListener("submit", handleFormSubmit);
+  }
