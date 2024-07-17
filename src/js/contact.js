5ff542ee10ac463071b304774a1ed025daaf684b
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-07-17 13:31:56 +0200

    @contacts.html added contactinformation

--- src/js/contact.js
+++ src/js/contact.js
@@ -73,7 +73,7 @@ async function postData(contact) {
 }
 
 function addContact(){
-  let btn = document.getElementById('contactForm');
+  let btn = document.getElementById('add-contact-section');
   btn.classList.remove('d-none');
 }
 
