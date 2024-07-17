99fb13cf41ffa126547e7cd8796be44295b07a51
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-07-17 09:56:31 +0200

    @contact.js check if contact exist

--- src/js/contact.js
+++ src/js/contact.js
@@ -26,15 +26,29 @@ function updateContacts(responseToJson) {
   let keys = Object.keys(responseToJson);
   for (let i = 0; i < keys.length; i++) {
     let contact = responseToJson[keys[i]];
-    allContacts.names.push(contact.name);
-    allContacts.mails.push(contact.mail);
-    allContacts.phones.push(contact.phone);
-    allContacts.images.push(contact.img);
+    if (!isContactExisting(contact)) {
+      allContacts.names.push(contact.name);
+      allContacts.mails.push(contact.mail);
+      allContacts.phones.push(contact.phone);
+      allContacts.images.push(contact.img);
+    } else {
+      console.log(`Kontakt ${contact.name} ist bereits vorhanden.`);
+    }
   }
   console.log(allContacts);
 }
 
+function isContactExisting(contact) {
+  return (
+    allContacts.names.includes(contact.name) &&
+    allContacts.mails.includes(contact.mail) &&
+    allContacts.phones.includes(contact.phone) &&
+    allContacts.images.includes(contact.img)
+  );
+}
+
 async function postData(contact) {
+
   try {
     let response = await fetch(BASE_URL + ".json", {
       method: "POST",
@@ -54,25 +68,26 @@ async function postData(contact) {
 
     await getData("/");
   } catch (error) {
-    console.error("error");
+    console.error("Fehler beim Hochladen:", error);
   }
 }
-function handleFormSubmit(event) {
-    event.preventDefault();  
-  
 
-    const newContact = {
-      name: document.getElementById("name").value,
-      mail: document.getElementById("mail").value,
-      phone: document.getElementById("phone").value,
-      img: document.getElementById("img").value,
-    };
-  
-    postData(newContact);
-    document.getElementById("contactForm").innerHTML ="";
-  }
-  
-  function setupFormListener() {
-    const form = document.getElementById("contactForm");
-    form.addEventListener("submit", handleFormSubmit);
-  }
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const newContact = {
+    name: document.getElementById("name").value,
+    mail: document.getElementById("mail").value,
+    phone: document.getElementById("phone").value,
+    img: document.getElementById("img").value,
+  };
+
+  postData(newContact);
+  document.getElementById("contactForm").innerHTML = "";
+}
+
+function setupFormListener() {
+  const form = document.getElementById("contactForm");
+  form.addEventListener("submit", handleFormSubmit);
+}
