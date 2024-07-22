63db4247d620cacc667a5bb654a22e0b1c5b78bc
M1roel <157500795+M1roel@users.noreply.github.com>
2024-07-22 14:50:55 +0200

    save the firebase init data in a seperate file and add the global variables in the signup.js

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDZv_Y7rO7cD6zajbn8N2xjTCRXirodJ_w",
-  authDomain: "join-19628.firebaseapp.com",
-  databaseURL: "https://join-19628-default-rtdb.firebaseio.com",
-  projectId: "join-19628",
-  storageBucket: "join-19628.appspot.com",
-  messagingSenderId: "828382956671",
-  appId: "1:828382956671:web:f180ef915a3607d86bc34a",
+  apiKey: FB_API_KEY,
+  authDomain: FB_AUTH_DOMAIN,
+  databaseURL: FB_DATABASE_URL,
+  projectId: FB_PROJECT_ID,
+  storageBucket: FB_STORAGE_BUCKET,
+  messagingSenderId: FB_MESSAGING_SENDER_ID,
+  appId: FB_APP_ID,
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
