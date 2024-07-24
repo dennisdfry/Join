26b2753e8c3e64e0e4a4ddd59ffb64f34ad2d099
M1roel <ppfautsch@yahoo.de>
2024-07-24 18:13:43 +0200

    Greeting changes depending on the time of day: -hour 6 - 11 = Good morning -hour 12 - 17 = Good afternoon -hour 18 - 5 = Good evening

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,9 +1,22 @@
 function init() {
     currentDate();
+    currentHour();
 }
 
 function currentDate() {
     const options = { day: 'numeric', month: 'long', year: 'numeric' };
     const date = new Date().toLocaleDateString('de-DE', options);
     document.querySelector('.summary-tasks-mid-right-date').innerHTML = date;
+}
+
+function currentHour() {
+    const hour = new Date().getHours();
+
+    if(hour > 6 && hour < 12) {
+    document.getElementById('greeting').innerHTML += `Good morgning,`
+    } else if (hour > 12 && hour < 18) {
+        document.getElementById('greeting').innerHTML += `Good afternoon,`
+    } else {
+        document.getElementById('greeting').innerHTML += `Good evening,`
+    }
 }
