cc56d6e3df78522a1e86a20ef6579128c01e661b
M1roel <ppfautsch@yahoo.de>
2024-07-25 17:09:42 +0200

    first step to solve problem with greeting in summary page

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,21 +1,25 @@
-function init() {
-    currentHour();
-}
-
 function currentDate() {
     const options = { day: 'numeric', month: 'long', year: 'numeric' };
     const date = new Date().toLocaleDateString('de-DE', options);
     document.querySelector('.summary-tasks-mid-right-date').innerHTML = date;
 }
 
-function currentHour() {
+function summaryGreeting() {
     const hour = new Date().getHours();
+    const greetingElement = document.querySelector('.summary-user-greeting');
 
-    if(hour > 6 && hour < 12) {
-    document.getElementById('greeting').innerHTML += `Good morgning,`
-    } else if (hour > 12 && hour < 18) {
-        document.getElementById('greeting').innerHTML += `Good afternoon,`
+    if (greetingElement) {
+        let greetingMessage = '';
+
+        if (hour > 6 && hour < 12) {
+            greetingMessage = 'Good morning,';
+        } else if (hour >= 12 && hour < 18) {
+            greetingMessage = 'Good afternoon,';
+        } else {
+            greetingMessage = 'Good evening,';
+        }
+        greetingElement.innerText += ` ${greetingMessage}`;
     } else {
-        document.getElementById('greeting').innerHTML += `Good evening,`
+        console.error('Element with class "summary-user-greeting" not found.');
     }
 }
