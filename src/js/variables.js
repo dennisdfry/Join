ea33a939765ea87fb51a2ca3ef014801b93e6742
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-08-18 19:12:41 +0200

    changed var

--- src/js/variables.js
+++ src/js/variables.js
@@ -5,6 +5,3 @@ const confirmPasswordInput = document.getElementById('confirm-password');
 const submitButton = document.getElementById('success');
 const togglePassword = document.getElementById('toggle-password');
 const toggleConfirmPassword = document.getElementById('toggle-confirm-password');
-const ACTIVE_CLASS = "active-contact";
-const CONTACTS_URL = "https://join-19628-default-rtdb.firebaseio.com/contacts";
-const HEADERS = { "Content-Type": "application/json" };
