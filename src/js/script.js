6782f2d59d2ee34013db67377f78116f6c0434e9
Dennis Freyer <dennis.dfry@gmail.com>
2024-09-06 13:45:26 +0200

    tileUserImage update

--- src/js/script.js
+++ src/js/script.js
@@ -133,17 +133,12 @@ async function searchIndexUrl(index, users, fetchImage) {
  * @param {number} index - The index of the task.
  */
 function tileUserImage(index) {
-  const container = document.getElementById(`userImageBoard${index}`);
-  const images = container.getElementsByClassName("image-div");
-  const containerWidth = 80;
-  const imageWidth = 32;
-  const imagelength = images.length;
-  const totalImagesWidth = imageWidth * imagelength;
-  const overlap = totalImagesWidth > containerWidth ? (totalImagesWidth - containerWidth) / (imagelength - 1) : 0;
-  for (let i = 0; i < images.length; i++) {
+  const images = document.getElementById(`userImageBoard${index}`).getElementsByClassName("image-div");
+const overlap = images.length * 32 > 80 ? (images.length * 32 - 80) / (images.length - 1) : 0;
+ for (let i = 0; i < images.length; i++) {
     const imagePosition = images[i];
     imagePosition.style.position = "absolute";
-    imagePosition.style.left = `${i * (imageWidth - overlap)}px`;
+    imagePosition.style.left = `${i * (32 - overlap)}px`;
   }
 }
 
