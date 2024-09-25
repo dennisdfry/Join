9530c364a8d649bcd885a2b6df04d6ba6e627a54
M1roel <ppfautsch@yahoo.de>
2024-09-25 06:57:00 +0200

    optimize links for privacy policy and legal notice on login sites

---
+++ src/js/link.js
@@ -0,0 +1,15 @@
+function goToPrivacy() {
+    window.location.replace("/public/login-datenschutz.html");
+  }
+  
+  function goToLegalNotice() {
+    window.location.replace("/public/login-impressum.html");
+  }
+  
+  function goToSignUp() {
+    location.replace("/public/signup.html");
+  }
+  
+  function goToSummary() {
+    location.replace("/public/index.html");
+  }
